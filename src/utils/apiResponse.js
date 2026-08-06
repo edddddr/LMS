80a750aa452d800b46d0f3d7bbac92f82335e02c
edddddr <endt6342@gmail.com>
2026-08-06 08:25:25 -0600

    feat: utility function for api responses

---
+++ src/utils/apiResponse.js
@@ -0,0 +1,12 @@
+export const apiResponse = (
+    res,
+    statusCode,
+    message,
+    data = null
+) => {
+    return res.status(statusCode).json({
+        success: statusCode < 400,
+        message,
+        data,
+    });
+};
