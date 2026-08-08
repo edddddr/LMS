358cbf0eab59d9f1e389a6befc63a74f7fbf155c
edddddr <endt6342@gmail.com>
2026-08-08 07:03:22 -0600

    reaf: use promise.all for call database one time to two

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -2,15 +2,17 @@ import prisma from "../config/prisma.js";
 import { NotFoundError } from "../error/NotFoundError.js"
 
 export const getAllProducts = async (page, limit) => {
-    const products = await prisma.product.findMany({
+    const [products, total] = await Promise.all([
+    prisma.product.findMany({
         skip: (page - 1) * limit,
         take: limit,
         orderBy: {
-            createdAt: "desc"
-        }
-    });
-    
-    const total = await prisma.product.count();
+            createdAt: "desc",
+        },
+    }),
+
+        prisma.product.count(),
+    ]);
 
     const totalPages = Math.ceil(total / limit);
 
